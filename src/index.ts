91bb16fbee02e42dd07b870909959901f291fe5c
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-19 16:47:24 -0700

    launch project

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,15 @@ const app = express();
  *  App Configuration
  */
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
 /**
  * Server Activation
  */
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
