6cd6ee033ea58a3588dc3c1a352b63555667aef9
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-19 17:48:29 -0700

    add GET functions for controller

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -18,7 +18,33 @@ export const itemsRouter = express.Router();
 
 // GET items
 
+itemsRouter.get("/", async (req: Request, res: Response) => {
+    try {
+        const items: Item[] = await ItemService.findAll();
+        
+        res.status(200).send(items);
+    } catch(e) {
+        res.status(500).send(e.message);
+    }
+});
+
 // GET items/:id
+
+itemsRouter.get("/:id", async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+    
+    try {
+        const item: Item = await ItemService.find(id);
+        
+        if (item) {
+            return res.status(200).send(item);
+        }
+        
+        res.status(404).send("item not found");
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
 
 // POST items
 
