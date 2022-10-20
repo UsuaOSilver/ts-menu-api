871f17996764c6417fab093087765ce3a64874ab
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-19 17:16:22 -0700

    create Node.js service

---
+++ src/items/items.service.ts
@@ -0,0 +1,14 @@
+/**
+ *  Data Model Interfaces
+ */
+
+import { BaseItem, Item } from "./item.interface";
+import { Items } from "./items.interface"
+
+/**
+ *  In-Memory Store
+ */
+
+/**
+ *  Service Methods
+ */
