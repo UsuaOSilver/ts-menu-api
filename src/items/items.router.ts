3b7cb2a81756f62d96c88ba3e90e7fae64200547
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-19 17:58:27 -0700

    add PUT function

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -62,6 +62,25 @@ itemsRouter.post("/", async (req: Request, res: Response) => {
 
 // PUT items/:id
 
-
+itemsRouter.put("/:id", async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+    
+    try {
+        const itemUpdate: Item = req.body;
+        
+        const existingItem: Item = await ItemService.find(id);
+        
+        if (existingItem) {
+            const updatedItem = await ItemService.update(id, itemUpdate);
+            return res.status(200).json(updatedItem);
+        }
+        
+        const newItem = await ItemService.create(itemUpdate);
+        
+        res.status(201).json(newItem);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
 
 // DELETE items/:id
