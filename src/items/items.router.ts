74ca8f548cc731b185465b7712dfd14ce05333cb
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-10-19 17:24:34 -0700

    Create Express controller with architecture template

---
+++ src/items/items.router.ts
@@ -0,0 +1,21 @@
+/**
+ *  Required External Module and Interfaces
+ */
+
+/**
+ *  Router Definition
+ */
+
+/**
+ * Controller Definitions
+ */
+
+// GET items
+
+// GET items/:id
+
+// POST items
+
+// PUT items/:id
+
+// DELETE items/:id
